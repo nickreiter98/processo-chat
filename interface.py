98b85d17b300637ef1a5e08eb9bcd70d307a1e54
nickreiter98 <nick.reiter@hotmail.de>
2024-01-18 10:11:14 +0100

    added functionalities that past requests are inlcuded too and transmitted to openai api

--- interface.py
+++ interface.py
@@ -1,38 +1,59 @@
 import json
+import logging
+
 from openai import OpenAI
+from colorama import Fore, Back, Style
+
 from pydantic_classes import RequestDetails
 from process_concepts.process_visualization import visualize_process
 
+
 if __name__ == "__main__":
+    #logging.basicConfig(level=logging.INFO)
     client = OpenAI()
 
+    INSTRUCTION = "What Would you like to know today?"
+    messages = [{"role": "system", "content": INSTRUCTION}]
+
+    previous_question_answer = []
+
     while True:
-        user_input = input("Give me a prompt: ")
-        print("+++REQUEST STARTED+++")
+        prompt = input(Fore.GREEN + Style.BRIGHT + INSTRUCTION + Style.RESET_ALL)
+
+        for question, answer in previous_question_answer:
+            messages.append({"role": "user", "content": question})
+            messages.append({"role": "assistant", "content": answer})
+        messages.append({"role": "user", "content": prompt})
 
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo",
             functions=[RequestDetails.openai_schema],
-            messages=[
-                {"role": "user", "content": user_input},
-            ]
+            messages=messages
         )
 
         response = completion.choices[0].message
+        if response.content == None:
+            previous_question_answer.append((prompt, "extracted and passed information to inner functionalities"))
+        else:
+            previous_question_answer.append((prompt, response.content))
 
+        if len(previous_question_answer) > 10:
+            previous_question_answer = previous_question_answer.pop(0)
+
+        response = completion.choices[0].message
 
         try:
             dict_response = json.loads(response.function_call.arguments)
             
             if dict_response["request_type"] == "process_visualization":
-                print("+++START PROCESS VISUALIZATION+++")
+                logging.info("+++START PROCESS VISUALIZATION+++")
                 visualize_process(dict_response["underlying_data"])
-                print("+++END PROCESS VISUALIZATION+++")
+                logging.info("+++END PROCESS VISUALIZATION+++")
 
         except AttributeError:
             print(response.content)
 
         
 
-        print("+++REQUEST ENDED+++")
+        logging.info("+++REQUEST ENDED+++")
 
